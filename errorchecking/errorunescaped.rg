382e5f6b1ecf06a1420ed4106793b150dd173249
TechFish3000 <techfish3000@gmail.com>
2020-07-23 15:34:47 +0930

    v1.0.2

---
+++ errorchecking/errorunescaped.rg
@@ -0,0 +1,3 @@
+hello how do u do // lmao
+this "should" be epic
+lmao " unescaped go brrr // yep" // lmao
--- errorchecking/escaping.py
+++ errorchecking/escaping.py
@@ -3,36 +3,65 @@ text = ""
 rg_file = None
 paired_chars = ('\"\"', "\'\'", "()", "{}", "[]")
 quote_chars = ("\"", "\'")
+string_regions = []
+
+def greatest(liste:list):
+    high = 0
+    for i in liste:
+        if i > high:
+            high = i
+    return high
+
 def tests(fp: str):
     rg_file = open(fp, "r")
     text = rg_file.read()
     stripped_text = text.strip()
 
-def strip_comments(rg_file):
-    lines = rg_file.readlines()
-    for i in lines:
-        if i.count("\\") == 1 and i.index("\\") not in reg
-
+def strip_comments(text: str):
+    
+    regions = find_string_regions(text.strip())
+    print(regions)
+    print(text.count("//"))
+    excluded = []
+    for i in range(text.count("//")): 
+        start = text.index("//", greatest(excluded) + 1)
+        valid = True
+        for reg in regions:
+            if reg[0] < start < reg[1]:
+                valid = False
+        if valid:
+            text = text[:text.index("//", greatest(excluded) + 1)] + text[text.index("\n", text.index("//", greatest(excluded) + 1)):]
+        else:
+            excluded.append(text.index("//"))
+    print(excluded)
+    return text
+        
 def find_string_regions(stripped_text: str):
     if stripped_text.count("\"") % 2 == 1 or stripped_text.count("\'") % 2 == 1:
         exit("wrong num quotes")
     removedchars = 0
-    regions = []
+    string_regions = [] 
     temptext = stripped_text
     for char in quote_chars:
-        for i in range(stripped_text.count(char)):
-            regions.append((temptext.index(char), temptext.index(char, start=temptext.index(char) + 1)))
-            temptext = temptext[:temptext.index(char)] + temptext.index[temptext.index(char) + 1:]
-            temptext = temptext[:temptext.index(char)] + temptext.index[temptext.index(char) + 1:]
+        for i in range(stripped_text.count(char) // 2):
+            string_regions.append((temptext.index(char) + removedchars, temptext.index(char,temptext.index(char) + 1) + removedchars))
+            temptext = temptext[:temptext.index(char)] + temptext[temptext.index(char) + 1:]
+            temptext = temptext[:temptext.index(char)] + temptext[temptext.index(char) + 1:]
             removedchars += 2
         temptext = stripped_text
         removedchars = 0
-    return regions
-
+    return string_regions
 
 def escaped_parentheses(stripped_text: str):
     for i in paired_chars:
         if stripped_text.count(i[0]) == stripped_text.count(i[1]):
             pass
+#"hello\"mo\"yeet\'me\'"
+#"hello // hi \n hello"
 
-print(find_string_regions("hello\"mo\""))
+b = open("errorunescaped.rg", "r")
+print(strip_comments(b.read()))
+#print("hello // hi \nhello\n\"hi // \"\n")
+#print(find_string_regions("hello // hi \nhello\n\"hi // \"\n".strip()))
+#print(strip_comments("hello // hi \nhello\n\"hi // \"\n"))
+
